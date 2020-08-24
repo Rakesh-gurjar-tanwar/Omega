aec3d35f040bb7a6b78eba7f1a2296b9a57b1508
rakeshgurjartanwar <70100108+rakeshgurjartanwar@users.noreply.github.com>
2020-08-24 15:42:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+var const firebaseConfig = {
+  apiKey: "AIzaSyBpTZmvbB1L8f-pYt_yw1Wi-fu018LuaqU",
+  authDomain: "hacking-1e0d5.firebaseapp.com",
+  databaseURL: "https://hacking-1e0d5.firebaseio.com",
+  projectId: "hacking-1e0d5",
+  storageBucket: "hacking-1e0d5.appspot.com",
+  messagingSenderId: "90893344856",
+  appId: "1:90893344856:web:e6fa0b516911861b94ea2d",
+  measurementId: "G-03NGZGGX5C"
 };
 
      
